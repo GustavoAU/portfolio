624fe4c5f2a4351bc01b724d7bb256700526dda3
Gustavo <gustavor2186@gmail.com>
2024-08-12 18:41:31 -0500

    fix menu

--- navbar.js
+++ navbar.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector(".Header__fixed");
-  const hero = document.querySelector(".hero");
-
-  const heroHeight = hero.offsetHeight;
-
+  handleHeader();
   window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY;
-    if (scrollPosition > heroHeight) {
-      header.classList.remove("hidden");
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-      header.classList.add("hidden");
-    }
+    handleHeader();
   });
 });
+
+const handleHeader = () => {
+  console.log("Gustavo");
+  const header = document.querySelector(".Header__fixed");
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 300) {
+    header.classList.remove("hidden");
+  } else {
+    header.classList.add("hidden");
+  }
+};
