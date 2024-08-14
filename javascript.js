b2a1cc2dc74ad2f24cbbee026eb5c336e9d0d313
Gustavo <gustavor2186@gmail.com>
2024-08-14 12:42:19 -0500

    finish header & hero with responsive

---
+++ javascript.js
@@ -0,0 +1,99 @@
+let hamburger = document.getElementById("hamburger");
+let menuNavigation = document.getElementById("Menu__navigation");
+let overlayNavigation = document.getElementById("overlay-navigation");
+let headerFixed = document.getElementById("Header__fixed");
+let body = document.querySelector("body");
+let menuContact = document.getElementById("Menu__contact");
+let overlayContact = document.getElementById("overlay__contact");
+
+document.getElementById("hamburger").addEventListener("click", () => {
+  hamburger.classList.toggle("cross");
+  menuNavigation.classList.toggle("cross");
+  menuNavigation.classList.toggle("hidden");
+  overlayNavigation.classList.toggle("hidden");
+  body.classList.toggle("overflow");
+  headerFixed.classList.add("hidden");
+});
+
+document.getElementById("Header__contact").addEventListener("click", () => {
+  hamburger.classList.remove("cross");
+  menuNavigation.classList.add("hidden");
+  menuContact.classList.toggle("hidden");
+  overlayNavigation.classList.add("hidden");
+  overlayContact.classList.toggle("hidden");
+  headerFixed.classList.add("hidden");
+  body.classList.add("overflow");
+});
+
+document
+  .getElementById("Header__contact-menu")
+  .addEventListener("click", () => {
+    hamburger.classList.remove("cross");
+    menuNavigation.classList.add("hidden");
+    menuContact.classList.toggle("hidden");
+    overlayNavigation.classList.add("hidden");
+    overlayContact.classList.toggle("hidden");
+    headerFixed.classList.add("hidden");
+    body.classList.add("overflow");
+  });
+
+document.getElementById("close-contact").addEventListener("click", () => {
+  menuContact.classList.add("hidden");
+  overlayContact.classList.add("hidden");
+  body.classList.remove("overflow");
+});
+
+document.getElementById("overlay__contact").addEventListener("click", () => {
+  menuContact.classList.add("hidden");
+  overlayContact.classList.add("hidden");
+  body.classList.remove("overflow");
+});
+
+document.getElementById("overlay-navigation").addEventListener("click", () => {
+  menuNavigation.classList.remove("cross");
+  menuNavigation.classList.add("hidden");
+  hamburger.classList.remove("cross");
+  overlayNavigation.classList.add("hidden");
+  body.classList.remove("overflow");
+});
+
+document.getElementById("close-navigation").addEventListener("click", () => {
+  menuNavigation.classList.remove("cross");
+  hamburger.classList.remove("cross");
+  overlayNavigation.classList.add("hidden");
+  menuNavigation.classList.add("hidden");
+  body.classList.remove("overflow");
+});
+
+document.getElementById("hamburger-fixed").addEventListener("click", () => {
+  hamburger.classList.toggle("cross");
+  menuNavigation.classList.toggle("cross");
+  menuNavigation.classList.toggle("hidden");
+  overlayNavigation.classList.toggle("hidden");
+  body.classList.toggle("overflow");
+  headerFixed.classList.add("hidden");
+});
+
+document
+  .getElementById("Header__contact-fixed")
+  .addEventListener("click", () => {
+    hamburger.classList.remove("cross");
+    menuNavigation.classList.add("hidden");
+    menuContact.classList.remove("hidden");
+    overlayNavigation.classList.add("hidden");
+    overlayContact.classList.toggle("hidden");
+    headerFixed.classList.add("hidden");
+    body.classList.add("overflow");
+  });
+
+document
+  .getElementById("Header__contact-bar-nav")
+  .addEventListener("click", () => {
+    hamburger.classList.remove("cross");
+    menuNavigation.classList.add("hidden");
+    menuContact.classList.toggle("hidden");
+    overlayNavigation.classList.add("hidden");
+    overlayContact.classList.toggle("hidden");
+    headerFixed.classList.add("hidden");
+    body.classList.add("overflow");
+  });
